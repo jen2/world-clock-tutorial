de12928c888f033e0d57eab8cc9d46a33bb2225c
jen2 <sipila.jen@gmail.com>
2017-05-07 13:49:50 -0400

    Completely updated project with better implementation

--- DateAndTime/ViewController.m
+++ DateAndTime/ViewController.m
@@ -10,247 +10,120 @@
 
 @interface ViewController ()
 
-@property (weak, nonatomic) IBOutlet UIButton *NYCLabel;
+@property (nonatomic, weak) IBOutlet UIButton *nyc;
+@property (nonatomic, weak) IBOutlet UIButton *paris;
+@property (nonatomic, weak) IBOutlet UIButton *moscow;
+@property (nonatomic, weak) IBOutlet UIButton *hongKong;
+@property (nonatomic, weak) IBOutlet UIButton *honolulu;
+@property (nonatomic, weak) IBOutlet UIButton *seattle;
+@property (nonatomic, weak) IBOutlet UILabel *timeLabel;
+@property (nonatomic, weak) IBOutlet UILabel *dateLabel;
 
-@property (weak, nonatomic) IBOutlet UIButton *parisLabel;
-
-@property (weak, nonatomic) IBOutlet UIButton *moscowLabel;
-
-@property (weak, nonatomic) IBOutlet UIButton *hongKongLabel;
-
-@property (weak, nonatomic) IBOutlet UIButton *honoluluLabel;
-
-@property (weak, nonatomic) IBOutlet UIButton *seattleLabel;
-
-@property(nonatomic, strong)NSArray *labels;
-
-@property(nonatomic, strong)NSDateComponents *components;
-
-//Labels for UI
-
-@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
-
-@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
-
-
-@property(strong, nonatomic)NSArray *dateAndTime;
-
-@property(strong, nonatomic)NSArray *dateAndTimeStrings;
-
-
-@property(strong, nonatomic)NSTimer * timer;
-
+@property(nonatomic, strong)NSString *timeZone;
+@property(nonatomic, strong)UIButton *selectedButton;
+@property(nonatomic, strong)NSTimer * timer;
 
 @end
 
 @implementation ViewController
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    
-    
-    self.labels = @[self.NYCLabel, self.parisLabel, self.moscowLabel, self.hongKongLabel, self.honoluluLabel, self.seattleLabel];
-    
-    for (UIButton *button in self.labels)
-    {
-        UIColor *customBlue = [UIColor colorWithRed:0.0 green:200.0 blue:235.0 alpha:1.0];
-        
-        [button setTitleColor:customBlue forState:UIControlStateNormal];
-        
-        
-        //[button setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
-        
-        
+    - (void)viewDidLoad {
+        [super viewDidLoad];
+        [self.nyc sendActionsForControlEvents: UIControlEventTouchUpInside];
+        [self highlightSelected]; ///
     }
 
-    [self.NYCLabel setTitleColor:[UIColor yellowColor] forState:UIControlStateNormal];
-    
-    NSLog(@"timezones: %@", [NSTimeZone abbreviationDictionary]);
-    
-    [self.NYCLabel sendActionsForControlEvents: UIControlEventTouchUpInside]; //Artificially touches button.
-    
-    
-}
-
-
--(NSArray *)getCurrentDateAndTimeWithFormattingForTimeZone: (NSString *)timeZone
-{
-    //NSString *timeZoneAbreviation = @"GMT";
-    
-    //Formatter, Locale & Time Zone For Date and Time.
-    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-    NSDateFormatter *timeFormatter = [[NSDateFormatter alloc] init];
-    NSLocale *posix = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
-    NSTimeZone *localTimeZone = [NSTimeZone timeZoneWithAbbreviation:timeZone];
-    
-    
-    [dateFormatter setLocale:posix];
-    [dateFormatter setDateFormat:@"EEEE MMMM dd y"];
-    [dateFormatter setTimeZone:localTimeZone];
-    
-    [timeFormatter setLocale:posix];
-    [timeFormatter setDateFormat:@"h:mm:ss a"];
-    [timeFormatter setTimeZone:localTimeZone];
-    
-    
-    //NSDate point in time.
-    NSDate *now = [NSDate date];
-    
-    NSString *date = [dateFormatter stringFromDate:now];
-    
-    NSString *time = [timeFormatter stringFromDate:now];
-    
-    NSLog(@"%@", date);
-    NSLog(@"%@", time);
-    
-    self.dateAndTimeStrings = @[date, time];
-
-    
-    return self.dateAndTimeStrings;
-    
-    
-}
-
-
-- (IBAction)newYorkCityButton:(id)sender
-{
-    
-    
-    [self.timer invalidate];
-    self.timer = nil;
-    
-    
-    
-    [self setAllLabelsToBlueExceptSelected:self.NYCLabel];
-    
-    NSArray *newYorkDateAndTimeStrings = [self getCurrentDateAndTimeWithFormattingForTimeZone: @"EDT"];
-    
-    self.dateAndTimeStrings = newYorkDateAndTimeStrings;
-    
-    
-    
-    self.timeLabel.text = newYorkDateAndTimeStrings[1];
-    
-    self.dateLabel.text = newYorkDateAndTimeStrings[0];
-    
-    
-    self.timer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(newYorkCityButton:) userInfo:nil repeats:YES];
-    
-    
-}
-- (IBAction)parisButton:(id)sender
-{
-    
-    [self.timer invalidate];
-    self.timer = nil;
-
-    
-    
-    [self setAllLabelsToBlueExceptSelected:self.parisLabel];
-    
-    
-    NSArray *parisDateAndTimeStrings = [self getCurrentDateAndTimeWithFormattingForTimeZone: @"CEST"];
-    
-    self.timeLabel.text = parisDateAndTimeStrings[1];
-    
-    self.dateLabel.text = parisDateAndTimeStrings[0];
-    
-    self.timer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(parisButton:) userInfo:nil repeats:YES];
-
-}
-
-
-- (IBAction)moscowButton:(id)sender
-{
-    [self.timer invalidate];
-    self.timer = nil;
-
-    
-    [self setAllLabelsToBlueExceptSelected:self.moscowLabel];
-    
-    NSArray *moscowDateAndTimeStrings = [self getCurrentDateAndTimeWithFormattingForTimeZone: @"MSD"];
-    
-    self.timeLabel.text = moscowDateAndTimeStrings[1];
-    
-    self.dateLabel.text = moscowDateAndTimeStrings[0];
-    
-    
-    self.timer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(moscowButton:) userInfo:nil repeats:YES];
-    
-}
-
-
-- (IBAction)hongKongButton:(id)sender
-{
-    [self.timer invalidate];
-    self.timer = nil;
-
-
-    [self setAllLabelsToBlueExceptSelected:self.hongKongLabel];
-    
-    NSArray *hongKongDateAndTimeStrings = [self getCurrentDateAndTimeWithFormattingForTimeZone: @"HKT"];
-    
-    self.timeLabel.text = hongKongDateAndTimeStrings[1];
-    
-    self.dateLabel.text = hongKongDateAndTimeStrings[0];
-    
-    self.timer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(hongKongButton:) userInfo:nil repeats:YES];
-    
-    
-}
-
-
-- (IBAction)honoluluButton:(id)sender
-{
-    [self.timer invalidate];
-    self.timer = nil;
-
-
-    [self setAllLabelsToBlueExceptSelected:self.honoluluLabel];
-    
-    NSArray *honoluluDateAndTimeStrings = [self getCurrentDateAndTimeWithFormattingForTimeZone: @"HST"];
-    
-    self.timeLabel.text = honoluluDateAndTimeStrings[1];
-    
-    self.dateLabel.text = honoluluDateAndTimeStrings[0];
-    
-    self.timer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(honoluluButton:) userInfo:nil repeats:YES];
-    
-    
-}
-
-
-- (IBAction)seattleButton:(id)sender
-{
-    [self.timer invalidate];
-    self.timer = nil;
-
-    [self setAllLabelsToBlueExceptSelected:self.seattleLabel];
-    
-    NSArray *seattleDateAndTimeStrings = [self getCurrentDateAndTimeWithFormattingForTimeZone: @"PST"];
-    
-    self.timeLabel.text = seattleDateAndTimeStrings[1];
-    
-    self.dateLabel.text = seattleDateAndTimeStrings[0];
-    
-    self.timer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(seattleButton:) userInfo:nil repeats:YES];
-    
-}
-
-
--(void)setAllLabelsToBlueExceptSelected:(UIButton *) buttonLabel
-{
-    
-    for (UIButton *button in self.labels)
-    {
-        UIColor *customBlue = [UIColor colorWithRed:0.0 green:200.0 blue:235.0 alpha:1.0];
-        
-        [button setTitleColor:customBlue forState:UIControlStateNormal];
+    -(IBAction)cityButtonTapped:(UIButton *)sender {
+        switch (sender.tag) {
+            case 100:
+                self.timeZone = @"EST";
+                self.selectedButton = self.nyc;
+                break;
+                
+            case 101:
+                self.timeZone = @"CEST";
+                self.selectedButton = self.paris;
+                break;
+                
+            case 102:
+                self.timeZone = @"MSD";
+                self.selectedButton = self.moscow;
+                break;
+                
+            case 103:
+                self.timeZone = @"HKT";
+                self.selectedButton = self.hongKong;
+                break;
+                
+            case 104:
+                self.timeZone = @"HST";
+                self.selectedButton = self.honolulu;
+                break;
+                
+            case 105:
+                self.timeZone = @"PST";
+                self.selectedButton = self.seattle;
+                break;
+                
+            default:
+                self.timeZone = @"EST";
+                self.selectedButton = self.nyc;
+                break;
+        }
+        [self setTappedCityTimer];
     }
-    
-    [buttonLabel setTitleColor:[UIColor yellowColor] forState:UIControlStateNormal];
-}
+
+    -(void)setTappedCityTimer {
+        [self.timer invalidate];
+        self.timer = nil;
+        
+        [self highlightSelected];///
+        [self unhighlightDeselected];/////
+        
+        [self setDateTimeLabelsWithTimeZone];
+        self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(setDateTimeLabelsWithTimeZone) userInfo:nil repeats:YES];
+    }
+
+    -(void)setDateTimeLabelsWithTimeZone {
+        NSArray *dateAndTime = [self formatCurrentDateTimeForTimeZone];
+        self.timeLabel.text = dateAndTime[1];
+        self.dateLabel.text = dateAndTime[0];
+    }
+
+     -(NSArray *)formatCurrentDateTimeForTimeZone {
+         NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+         NSDateFormatter *timeFormatter = [[NSDateFormatter alloc] init];
+         NSLocale *posix = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
+         NSTimeZone *localTimeZone = [NSTimeZone timeZoneWithAbbreviation:self.timeZone];
+         [dateFormatter setLocale:posix];
+         [dateFormatter setDateFormat:@"EEEE MMMM dd y"];
+         [dateFormatter setTimeZone:localTimeZone];
+         [timeFormatter setLocale:posix];
+         [timeFormatter setDateFormat:@"h:mm:ss a"];
+         [timeFormatter setTimeZone:localTimeZone];
+         
+         NSDate *now = [NSDate date];
+         NSString *date = [dateFormatter stringFromDate:now];
+         NSString *time = [timeFormatter stringFromDate:now];
+         
+         NSArray *formattedDateAndTime = @[date, time];
+         return formattedDateAndTime;
+     }
 
 
+    - (void)highlightSelected { /////
+        self.selectedButton.tintColor = [UIColor yellowColor];
+    }
+
+    -(void)unhighlightDeselected { /////
+        NSMutableArray *cities = [@[self.nyc, self.paris, self.moscow, self.hongKong, self.honolulu, self.seattle]mutableCopy];
+        NSMutableArray *unselectedCities = [NSMutableArray array];
+        
+        for (UIButton *city in cities) {
+            if (city != self.selectedButton) {
+                [unselectedCities addObject:city];
+            }
+        }
+        [unselectedCities setValue:[UIColor blueColor] forKey:@"tintColor"];
+    }
+        
 @end
